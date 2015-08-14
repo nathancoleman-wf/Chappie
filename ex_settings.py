c2956f747a6d40a2a7f85a6a6c4ac899749783b7
Nathan Coleman <nathan.coleman@workiva.com>
2015-08-14 14:05:36 -0400

    Add example settings.py

---
+++ ex_settings.py
@@ -0,0 +1,11 @@
+from lib import User
+
+USERS = [User('Real Name', 'slack username', 'github username')]
+
+# Slack Settings
+SLACK_TOKEN = 'token'
+SLACK_ROOM_NAME = 'room name for notifications'
+
+# GitHub Settings
+GITHUB_USERNAME = 'github username'
+GITHUB_PASSWORD = 'github password'
